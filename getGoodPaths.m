c299eb600d25f6c72bf5d1413bc06dd5ad7b25c3
Zach <zvickland@gmail.com>
2021-03-07 14:51:13 -0700

    Updated to output total tx and rx. Need to readd bw per link

--- getGoodPaths.m
+++ getGoodPaths.m
@@ -1,4 +1,4 @@
-function [goodPaths, bwUsage] = getGoodPaths(src, dest, linkMatrix)
+function [goodPaths, totalTx, totalRx] = getGoodPaths(src, dest, linkMatrix)
 % Finds all links from src to dest without duplicates
 % 
 % Input
@@ -8,19 +8,30 @@ function [goodPaths, bwUsage] = getGoodPaths(src, dest, linkMatrix)
 %   linkMatrix - nxn link matrix (non-zero is link). n must be > src & dest
 % 
 % Output
-%   bwUsage - nxn link matrix indicating bandwidth used over each link.
-%       Note, this is actually directional (non-symmetric)
+%   totalTx - nx1 vector indicating how much each node transmits
+%   totalRx - nx1 vector indicating how much each node receives
 % 
 % Test
+% % simple 1 path
 % linkMatrix = [0 1 0; 1 0 1; 0 1 0]; 
-% [goodPaths, bwUsage] = getGoodPaths(3, 1, linkMatrix)
-% linkMatrix = [0 1 0; 1 0 1; 0 1 0]; 
-% [goodPaths, bwUsage] = getGoodPaths(3, 1, linkMatrix)
+% [goodPaths, totalTx, totalRx] = getGoodPaths(3, 1, linkMatrix)
+% % simple 2 path
+% linkMatrix = [0 1 1; 1 0 1; 1 1 0]; 
+% [goodPaths, totalTx, totalRx] = getGoodPaths(3, 1, linkMatrix)
+% % complex 2 path
+% linkMatrix = [0 1 1 0 0;
+%     1 0 0 1 0; 
+%     1 0 0 0 1;
+%     0 1 0 0 1;
+%     0 0 1 1 0]; 
+% [goodPaths, totalTx, totalRx] = getGoodPaths(1, 5, linkMatrix)
 % 
 % History
 % Created 3/27/2021 ZV - helper for routeDiscoveryPhase
 
-bwUsage = zeros(size(linkMatrix));
+numNodes = size(linkMatrix, 1);
+totalTx = zeros(numNodes, 1);
+totalRx = zeros(numNodes, 1);
 
 msgSize = 500;
 
@@ -40,7 +51,7 @@ while numel(paths) > 0
         paths = paths(2:end);
     end
     
-    if ~isempty(find(myPath == dest)) 
+    if ~isempty(find(myPath == dest, 1)) 
         %Made it to destination!
         goodPaths{numel(goodPaths) + 1} = myPath;
     elseif numel(myPath) > numel(unique(myPath))
@@ -49,7 +60,8 @@ while numel(paths) > 0
     else
         %retransmit
         neighbors = find(linkMatrix(thisNode,:));
-        bwUsage(thisNode, neighbors) = bwUsage(thisNode, neighbors) + 500;
+        totalTx(thisNode) = totalTx(thisNode) + msgSize;
+        totalRx(neighbors) = totalRx(neighbors) + 500;
         for ii = 1:numel(neighbors)
             paths{numel(paths) + 1} = [myPath, neighbors(ii)];
         end
